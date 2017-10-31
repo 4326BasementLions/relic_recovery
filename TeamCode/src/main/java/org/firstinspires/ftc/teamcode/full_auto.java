1e38a51c575dfc62aaee75dcdfa2f4fc8bea832f
Eleanor Mather <eleanor_mather@horacemann.org>
2017-10-30 21:11:32 -0400

    added imu stuff in comments + idea of method
    method will most certainly change after we see what the imu telemetry says after callibration

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/full_auto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/full_auto.java
@@ -52,6 +52,23 @@ import org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer;
 import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackable;
 import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackableDefaultListener;
 import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackables;
+
+import com.qualcomm.hardware.bosch.BNO055IMU;
+import com.qualcomm.hardware.bosch.JustLoggingAccelerationIntegrator;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.robotcore.external.Func;
+import org.firstinspires.ftc.robotcore.external.navigation.Acceleration;
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
+import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
+import org.firstinspires.ftc.robotcore.external.navigation.Position;
+import org.firstinspires.ftc.robotcore.external.navigation.Velocity;
+
+import java.util.Locale;
 /**
  * This file illustrates the concept of driving a path based on encoder counts.
  * It uses the common Pushbot hardware class to define the drive on the robot.
@@ -113,9 +130,16 @@ public class auto extends LinearOpMode {
 
 
     VuforiaLocalizer vuforia;
+    
+    //BNO055IMU imu; //Sensor itself
+    
+    //Orientation angles // xyz axes in telemetry
+    //Acceleration gravity;
+    // int optimalAngle=90;
 
     @Override
     public void runOpMode() {
+        
 
         /*
          * Initialize the drive system variables.
@@ -168,10 +192,31 @@ public class auto extends LinearOpMode {
         VuforiaTrackables relicTrackables = this.vuforia.loadTrackablesFromAsset("RelicVuMark");
         VuforiaTrackable relicTemplate = relicTrackables.get(0);
         relicTemplate.setName("relicVuMarkTemplate"); // can help in debugging; otherwise not necessary
+        
+        /*BNO055IMU.Parameters parameters = new BNO055IMU.Parameters();
+        parameters.angleUnit           = BNO055IMU.AngleUnit.DEGREES; // angles told in degrees
+        parameters.accelUnit           = BNO055IMU.AccelUnit.METERS_PERSEC_PERSEC; //rate of acceleration but we're not using that
+        parameters.calibrationDataFile = "AdafruitIMUCalibration.json"; // see the calibration sample opmode
+        parameters.loggingEnabled      = true;
+        parameters.loggingTag          = "IMU"; //name of sensor on telemetry
+        parameters.accelerationIntegrationAlgorithm = new JustLoggingAccelerationIntegrator();*/
+        
+        //composeTelemetry();//defined below
 
+        
+        
 
         // Wait for the game to start (driver presses PLAY)
         waitForStart();
+        
+       /* // log current rate of acceleration
+        imu.startAccelerationIntegration(new Position(), new Velocity(), 1000);
+        
+               
+        // Loop and update the dashboard
+        while (opModeIsActive()) {
+            telemetry.update();
+        }*/
 
         // Step through each leg of the path,
         // Note: Reverse movement is obtained by setting a negative distance (not speed)
@@ -205,6 +250,10 @@ public class auto extends LinearOpMode {
         }
 
         strafeLeft(0.2, 0.3);
+        //Turn until at angle we want to be at(haven't tested yet so the input value may be totally wrong)
+        /*while(angles.thirdAngle!=optimalAngle){
+            turnLeft(1,1);        
+        }*/
 
         sleep(1000);
         relicTrackables.activate();
@@ -317,6 +366,71 @@ public class auto extends LinearOpMode {
             sleep(250);   // optional pause after each move
         }
     }
+   /* void composeTelemetry() {
+
+        // At the beginning of each telemetry update, grab a bunch of data
+        // from the IMU that we will then display in separate lines.
+        telemetry.addAction(new Runnable() { @Override public void run()
+                {
+                // Acquiring the angles is relatively expensive; we don't want
+                // to do that in each of the three items that need that info, as that's
+                // three times the necessary expense.
+                angles   = imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX,/*height, then relative front/back, then relative left/right AngleUnit.DEGREES);
+                gravity  = imu.getGravity();
+                }
+            });
+
+        telemetry.addLine()
+            .addData("status", new Func<String>() {
+                @Override public String value() {
+                    return imu.getSystemStatus().toShortString();
+                    }
+                })
+            .addData("calib", new Func<String>() {
+                @Override public String value() {
+                    return imu.getCalibrationStatus().toString();
+                    }
+                });
+
+        telemetry.addLine()
+            .addData("heading", new Func<String>() {
+                @Override public String value() {
+                    return formatAngle(angles.angleUnit, angles.firstAngle); //angle from straight z axis
+                    }
+                })
+            .addData("roll", new Func<String>() {
+                @Override public String value() {
+                    return formatAngle(angles.angleUnit, angles.secondAngle); //angle from straight y axis
+                    }
+                })
+            .addData("pitch", new Func<String>() {
+                @Override public String value() {
+                    return formatAngle(angles.angleUnit, angles.thirdAngle);//angle from straight x axis
+                    }
+                });
+
+        telemetry.addLine()
+            .addData("grvty", new Func<String>() {
+                @Override public String value() {
+                    return gravity.toString();
+                    }
+                })
+            .addData("mag", new Func<String>() {
+                @Override public String value() {
+                    return String.format(Locale.getDefault(), "%.3f",
+                            Math.sqrt(gravity.xAccel*gravity.xAccel  //relative right/left movement
+                                    + gravity.yAccel*gravity.yAccel  //relative front/back movement
+                                    + gravity.zAccel*gravity.zAccel)); //relative up/down movement
+                    }
+                });
+    }
+    String formatAngle(AngleUnit angleUnit, double angle) {
+        return formatDegrees(AngleUnit.DEGREES.fromUnit(angleUnit, angle));
+    }
+
+    String formatDegrees(double degrees){
+        return String.format(Locale.getDefault(), "%.1f", AngleUnit.DEGREES.normalize(degrees)); //just arranging how data is seen
+    }*/
 
     public void turnLeft(double power, double time) {
         encoderDrive(DRIVE_SPEED, power, power, power, power, time);
